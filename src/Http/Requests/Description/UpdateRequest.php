56012c681542a2c68a53fa1e21465159371bdef6
erenMustafaOzdal <eren.060737@gmail.com>
2016-12-07 03:34:17 +0000

    request

--- src/Http/Requests/Description/UpdateRequest.php
+++ src/Http/Requests/Description/UpdateRequest.php
@@ -2,10 +2,10 @@
 
 namespace ErenMustafaOzdal\LaravelDescriptionModule\Http\Requests\Description;
 
-use App\Http\Requests\Request;
+use ErenMustafaOzdal\LaravelModulesBase\Requests\BaseRequest;
 use Sentinel;
 
-class UpdateRequest extends Request
+class UpdateRequest extends BaseRequest
 {
     /**
      * Determine if the user is authorized to make this request.
